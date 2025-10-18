bab9075fcecd5a8c258df3bb2ff5e857b2863955
Santosh Sahoo <onlysantosh199@gmail.com>
2025-10-18 11:14:35 +0530

    stack part2

---
+++ stack2/stockSpan.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+    int arr[] = {100,80,60,70,60,75,85};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int ans[n];
+    ans[0] = 1;
+    stack<int> st;
+    st.push(0);
+    for(int i=1; i<n; i++){
+        while(st.size()>0 and arr[st.top()] <= arr[i]) st.pop();
+        if(st.size()==0) ans[i] = i+1;
+        else ans[i] = i-st.top();
+        st.push(i);
+    }
+    for(int i=0; i<n; i++){
+        cout<<ans[i]<<" ";
+    }
+}
